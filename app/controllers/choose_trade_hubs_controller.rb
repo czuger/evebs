f27d7df5084ec7da49ca09f138d6ef9ca6aa6c1a
ced <zuger.cedric@gmail;com>
2015-05-26 16:56:13 +0200

    Sort the trade hubs by alphabetical order.

--- app/controllers/choose_trade_hubs_controller.rb
+++ app/controllers/choose_trade_hubs_controller.rb
@@ -4,7 +4,7 @@ class ChooseTradeHubsController < ApplicationController
 
   def edit
     @user = current_user
-    @trade_hubs = TradeHub.all.to_a
+    @trade_hubs = TradeHub.all.to_a.sort
     @user_trade_hubs_ids = @user.trade_hubs.map{ |e| e.id }
     @per_group_count = (@trade_hubs.length/4.0).ceil
   end
