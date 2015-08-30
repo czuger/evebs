3aa85eab03d52f6d8a13440b726e1e88f78359d8
ced <zuger.cedric@gmail.com>
2015-08-30 12:33:26 +0200

    Add cleanup public schema.

---
+++ scripts/cleanup_public_schema.sql
@@ -0,0 +1,14 @@
+/* SELECT 'DROP TABLE ' || table_name || ';' FROM information_schema.tables WHERE table_schema='public'; */
+
+/*
+DROP TABLE trade_hubs_users;
+DROP TABLE identities;
+DROP TABLE materials;
+DROP TABLE min_prices;
+DROP TABLE sale_records;
+DROP TABLE schema_migrations;
+DROP TABLE stations;
+DROP TABLE trade_hubs;
+DROP TABLE trade_orders;
+DROP TABLE users;
+*/
