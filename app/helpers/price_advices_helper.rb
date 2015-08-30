aea36247850bade1fa8602431d644dde1c1dce95
ced <zuger.cedric@gmail.com>
2015-08-30 17:23:32 +0200

    Fixes #9

--- app/helpers/price_advices_helper.rb
+++ app/helpers/price_advices_helper.rb
@@ -1,7 +1,11 @@
 module PriceAdvicesHelper
   def print_isk(amount)
     if amount
-      number_to_currency(amount.round(1), unit: "ISK ", separator: ",", delimiter: " ", format: '%n %u')
+      if amount.class != String
+        number_to_currency(amount.round(1), unit: "ISK ", separator: ",", delimiter: " ", format: '%n %u')
+      else
+        amount
+      end
     end
   end
 end
