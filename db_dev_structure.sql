f0d79ea37186c25fff4d52bc29a4da60740e9956
Zuger Cédric <zuger.cedric@gmail.com>
2015-09-16 16:35:58 +0200

    Database modification

--- db_dev_structure.sql
+++ db_dev_structure.sql
@@ -1,8 +1,9 @@
 CREATE USER eve_business_server WITH CREATEDB UNENCRYPTED PASSWORD '123456';
 
-CREATE DATABASE eve_business_server WITH OWNER = eve_business_server;
+CREATE DATABASE eve_business_server_dev WITH OWNER = eve_business_server;
+CREATE DATABASE eve_business_server_test WITH OWNER = eve_business_server;
 
-\connect eve_business_server;
-
-CREATE SCHEMA dev AUTHORIZATION eve_business_server;
-CREATE SCHEMA test AUTHORIZATION eve_business_server;
+-- \connect eve_business_server;
+--
+-- CREATE SCHEMA dev AUTHORIZATION eve_business_server;
+-- CREATE SCHEMA test AUTHORIZATION eve_business_server;
