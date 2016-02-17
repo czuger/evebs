7699ba86fe61b1a8a481573ac189e99b6f68a0b3
ced <zuger.cedric@gmail.com>
2016-02-17 06:51:32 +0100

    Google implementation.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   # Thanks to : https://coderwall.com/p/bsfitw/ruby-on-rails-4-authentication-with-facebook-and-omniauth
   provider :facebook, '1423903911250248', 'b20bfe70879536e219da6820b8aea962'
+
+  provider :google_oauth2, '435800437430-kmgtgdlbr5jo53smnhiafe3cvvtbgglo.apps.googleusercontent.com', 'gjogipms3ECXLLSFv95VZZ48'
+
   provider :identity, on_failed_registration: lambda { |env|
     IdentitiesController.action(:new).call(env)
   }
