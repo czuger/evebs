2282a6d85f07acea6d6ec325a5605d2c2018bf14
ced <zuger.cedric@gmail.com>
2016-02-17 19:19:05 +0100

    Print nb items when computing min prices in Jita.

--- app/models/jita_margin.rb
+++ app/models/jita_margin.rb
@@ -5,6 +5,7 @@ class JitaMargin < ActiveRecord::Base
   def self.compute_margins
 
     jita = TradeHub.find_by_eve_system_id( Component::JITA_EVE_SYSTEM_ID )
+    items_processed = 0
     EveItem.find_each do |item|
 
       next if item.epic_blueprint
@@ -25,12 +26,13 @@ class JitaMargin < ActiveRecord::Base
         sum_volume = cplma ? cplma.volume_sum : -1
 
         jita_margin = JitaMargin.find_or_create_by!( eve_item_id: item.id )
-        result = jita_margin.update_attributes( margin: margin * batch_size,
+        jita_margin.update_attributes( margin: margin * batch_size,
           jita_min_price: min_price.min_price, cost: unit_cost,
           margin_percent: margin_percent, mens_volume: sum_volume, batch_size: batch_size )
 
-        dummy = true
+        items_processed += 1
       end
     end
+    puts "#{items_processed} items processed"
   end
 end
