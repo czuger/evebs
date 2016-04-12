9f02adc1fd6fb75fe4f0ff8f2761962b60eb93b9
Zuger Cédric <zuger.cedric@gmail.com>
2016-04-12 17:51:38 +0200

    Changing version number.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,10 +3,9 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   # Thanks to : https://coderwall.com/p/bsfitw/ruby-on-rails-4-authentication-with-facebook-and-omniauth
+
   provider :facebook, '1423903911250248', 'b20bfe70879536e219da6820b8aea962'
-
   provider :google_oauth2, '435800437430-kmgtgdlbr5jo53smnhiafe3cvvtbgglo.apps.googleusercontent.com', 'gjogipms3ECXLLSFv95VZZ48'
-
   provider :crest, '360a9dffb6fb4ade98945d9512c90f09', 'REyPAbCHUOw6fFanCDcdh4bz6jUpIk1tVWik22vA'
 
   provider :identity, on_failed_registration: lambda { |env|
