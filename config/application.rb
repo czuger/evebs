f8e98690e8741281b3f06412e32fc023f40b2ba7
Zuger Cédric <zuger.cedric@gmail.com>
2016-07-07 16:29:11 +0200

    Test for omniauth.

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,6 @@ module EveBusinessServer
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.force_ssl = true
-
     config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/"
 
     config.generators do |g|
