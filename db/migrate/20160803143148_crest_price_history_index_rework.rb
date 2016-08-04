ff1461a9fb63e579ba26891c308286d979af9851
Zuger Cédric <zuger.cedric@gmail.com>
2016-08-04 05:31:35 +0200

    Crest price history index rework.

---
+++ db/migrate/20160803143148_crest_price_history_index_rework.rb
@@ -0,0 +1,16 @@
+# TODO : rename that
+class CrestPriceHistoryIndexRework < ActiveRecord::Migration
+  def change
+
+    remove_column :crest_price_histories, :day_timestamp
+
+    remove_index :crest_price_histories, :eve_item_id
+    remove_index :crest_price_histories, :region_id
+    remove_index :crest_price_histories, :history_date
+    remove_index :crest_price_histories, name: :price_histories_all_keys_index
+
+    add_index :crest_price_histories, [ :region_id, :eve_item_id ], name: :index_crest_price_histories_on_region_and_item
+    add_index :crest_price_histories, :history_date
+
+  end
+end
