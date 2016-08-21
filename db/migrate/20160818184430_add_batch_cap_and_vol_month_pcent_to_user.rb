152c0a98dd20b5bdf3134fb950f7928a566100ec
ced <zuger.cedric@gmail.com>
2016-08-21 11:17:31 +0200

    Default values changed.

--- db/migrate/20160818184430_add_batch_cap_and_vol_month_pcent_to_user.rb
+++ db/migrate/20160818184430_add_batch_cap_and_vol_month_pcent_to_user.rb
@@ -1,6 +1,6 @@
 class AddBatchCapAndVolMonthPcentToUser < ActiveRecord::Migration
   def change
-    add_column :users, :batch_cap, :boolean, default: false, null: false
-    add_column :users, :vol_month_pcent, :integer, default: 20, null: false
+    add_column :users, :batch_cap, :boolean, default: true, null: false
+    add_column :users, :vol_month_pcent, :integer, default: 10, null: false
   end
 end
