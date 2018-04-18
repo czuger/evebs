da57d1d362fd1b9a253f510ae0fa2465f132a868
ced <zuger.cedric@gmail.com>
2018-04-18 08:08:52 +0200

    Fixing sessions tests.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -7,21 +7,21 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
       { :provider => 'twitter', :uid => '123545', 'info' => {'email' => 'testuser@testmail.com', 'name' => 'test', 'image' => ''},
       'credentials' => {'token' => '123456', 'expires_at' => 123456789 } } )
-    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
   end
 
   test 'should get new' do
-    get :new
+    get new_sessions_url
     assert_response :success
   end
 
   test 'should get create' do
-    post :create, provider: :twitter
+    post '/auth/twitter/callback'
     assert_redirected_to price_advices_advice_prices_url
   end
 
   test 'should get destroy' do
-    get :destroy
+    get signout_url
     assert_redirected_to root_url
   end
 
