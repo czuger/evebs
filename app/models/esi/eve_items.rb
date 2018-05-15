489d84c4685631e26965032cf2b40d0d48944c3d
ced <zuger.cedric@gmail.com>
2018-05-15 12:37:28 +0200

    Eve items full types download.

--- app/models/esi/eve_items.rb
+++ app/models/esi/eve_items.rb
@@ -1,29 +1,42 @@
 class Esi::EveItems < Esi::Download
 
   def initialize( debug_request: false )
-    super( nil, {}, debug_request: debug_request )
+    super( 'universe/types/', {}, debug_request: debug_request )
     # p @errors_limit_remain
   end
 
   def update
-    new_ids = TypeInRegion.distinct.pluck( :cpp_type_id )
+
+    new_ids = get_all_pages
     market_transformation_hash = Hash[ MarketGroup.pluck( :cpp_market_group_id, :id ) ]
 
-    new_ids.each do |missing_id|
-      @rest_url = "universe/types/#{missing_id}/"
-      cpp_item = get_page_retry_on_error
+    puts "About to download #{new_ids.count} items."
+    updated_items = downloaded_items = 0
 
-      next unless cpp_item['published']
+    ActiveRecord::Base.transaction do
+      new_ids.each do |missing_id|
+        @rest_url = "universe/types/#{missing_id}/"
+        cpp_item = get_page_retry_on_error
 
-      item = EveItem.where( cpp_eve_item_id: missing_id ).first_or_initialize do |i|
-        i.name = cpp_item['name']
-        i.name_lowcase = i.name.downcase
-        i.market_group_id = market_transformation_hash[ cpp_item['market_group_id'] ]
+        downloaded_items += 1
+
+        if downloaded_items % 1000 == 0
+          puts "Downloaded items : #{downloaded_items} / #{new_ids.count}."
+        end
+
+        next unless cpp_item['published']
+
+        item = EveItem.where( cpp_eve_item_id: missing_id ).first_or_initialize
+
+        item.name = cpp_item['name']
+        item.name_lowcase = item.name.downcase
+        item.market_group_id = market_transformation_hash[ cpp_item['market_group_id'] ]
+        item.save!
+
+        updated_items += 1
       end
-      item.save!
     end
 
-    puts "Items updated : #{new_ids.count}"
-
+    puts "Items downloaded : #{new_ids.count}, items updated : #{updated_items}."
   end
 end
