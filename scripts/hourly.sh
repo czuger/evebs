ed72510a723c27a5e3ad24097cdb3438c5415e4b
ced <zuger.cedric@gmail.com>
2018-05-31 12:52:37 +0200

    Email sent at the end of the hourly process.

--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -9,3 +9,5 @@ date >> log/hourly.log
 date >> log/hourly.err
 
 RAILS_ENV=production bundle exec rake data_compute:full:hourly >>log/hourly.log 2>>log/hourly.err
+
+cat log/hourly.log log/hourly.err | mail `cat config/email.txt` -s "Evebs staging hourly process"
