af288377be03f6fb22ec478c816312ed7c45dc89
ced <zuger.cedric@gmail.com>
2018-05-31 14:43:32 +0200

    Fixing cron script.

--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -8,6 +8,6 @@ cd $1
 date > log/hourly.log
 date > log/hourly.err
 
-RAILS_ENV=production bundle exec rake process:full:hourly >>log/hourly.log 2>>log/hourly.err
+RAILS_ENV=staging bundle exec rake process:full:hourly >>log/hourly.log 2>>log/hourly.err
 
 cat log/hourly.log log/hourly.err | mail `cat config/email.txt` -s "Evebs staging hourly process"
