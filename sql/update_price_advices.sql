458cf0fee000c80e8e6fec3eeb0936aea64e09ec
ced <zuger.cedric@gmail.com>
2018-06-09 12:15:00 +0200

    Min price update set updated_at field.

--- sql/update_price_advices.sql
+++ sql/update_price_advices.sql
@@ -15,31 +15,32 @@ INSERT INTO prices_advices( eve_item_id, region_id, trade_hub_id, created_at, up
 
 /* Update all datas */
 UPDATE prices_advices cpa
-SET vol_month = cplma.volume_sum, vol_day = floor( cplma.volume_sum / 30 ), avg_price = cplma.avg_price_avg
+SET vol_month = cplma.volume_sum, vol_day = floor( cplma.volume_sum / 30 ), avg_price = cplma.avg_price_avg,
+updated_at = now()
 FROM crest_prices_last_month_averages cplma, trade_hubs th
 WHERE cpa.eve_item_id = cplma.eve_item_id
 AND cpa.trade_hub_id = th.id
 AND th.region_id = cplma.region_id;
 
 UPDATE prices_advices cpa
-SET min_price = mp.min_price
+SET min_price = mp.min_price, updated_at = now()
 FROM min_prices mp
 WHERE cpa.eve_item_id = mp.eve_item_id
 AND cpa.trade_hub_id = mp.trade_hub_id;
 
 UPDATE prices_advices cpa
-SET cost = ei.cost
+SET cost = ei.cost, updated_at = now()
 FROM eve_items ei
 WHERE cpa.eve_item_id = ei.id;
 
 UPDATE prices_advices cpa
-SET full_batch_size = bp.nb_runs*bp.prod_qtt, prod_qtt = bp.prod_qtt
+SET full_batch_size = bp.nb_runs*bp.prod_qtt, prod_qtt = bp.prod_qtt, updated_at = now()
 FROM eve_items ei, blueprints bp
 WHERE cpa.eve_item_id = ei.id
 AND ei.id = bp.eve_item_id;
 
-UPDATE prices_advices cpa SET single_unit_cost = (cost*(1+0.11))/prod_qtt;
+UPDATE prices_advices cpa SET single_unit_cost = (cost*(1+0.11))/prod_qtt, updated_at = now();
 
 UPDATE prices_advices cpa
-SET daily_monthly_pcent = min_price / avg_price;
+SET daily_monthly_pcent = min_price / avg_price, updated_at = now();
 
