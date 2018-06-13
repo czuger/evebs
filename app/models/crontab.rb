9f7918e7ac754a87b0a366c92b02a3b500b7a3a6
ced <zuger.cedric@gmail.com>
2018-06-13 10:08:05 +0300

    Adding a crontab check for hourly process - bugfix.

--- app/models/crontab.rb
+++ app/models/crontab.rb
@@ -2,12 +2,17 @@ class Crontab < ApplicationRecord
 
   def self.start( cron_name )
     r = Crontab.where( cron_name: cron_name ).first_or_create!
-    exit if r.status
+
+    if r.status
+      Banner.p 'Process actually running. Exiting'
+      exit
+    end
+
     r.update!( status: true )
   end
 
-  def self.start( cron_name )
-    r = Crontab.where( cron_name: cron_name ).update_all( status: false )
+  def self.stop( cron_name )
+    Crontab.where( cron_name: cron_name ).update_all( status: false )
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_101819) do
+ActiveRecord::Schema.define(version: 2018_06_12_222810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,13 @@ ActiveRecord::Schema.define(version: 2018_06_11_101819) do
     t.datetime "updated_at", null: false
     t.index ["eve_item_id"], name: "index_crest_prices_last_month_averages_on_eve_item_id"
     t.index ["region_id"], name: "index_crest_prices_last_month_averages_on_region_id"
+  end
+
+  create_table "crontabs", force: :cascade do |t|
+    t.string "cron_name", null: false
+    t.boolean "status", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "eve_empty_market_histories", force: :cascade do |t|
