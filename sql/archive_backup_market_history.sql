6b1c760289dcc27438fceea3c64978e9c9249839
ced <zuger.cedric@gmail.com>
2018-06-24 09:21:56 +0200

    Archive full history table.

--- sql/archive_backup_market_history.sql
+++ sql/archive_backup_market_history.sql
@@ -4,14 +4,13 @@ INSERT INTO eve_market_history_archives
   to_char( history_date, 'MM' ), history_date, order_count,
            volume, low_price, avg_price, high_price, now(), now()
            FROM eve_markets_histories
-           WHERE history_date < now() - interval '2 months'
-           AND NOT EXISTS (
+           WHERE NOT EXISTS (
              SELECT id FROM eve_market_history_archives WHERE
                eve_market_history_archives.region_id = eve_markets_histories.region_id AND
                eve_market_history_archives.eve_item_id = eve_markets_histories.eve_item_id AND
                eve_market_history_archives.history_date = eve_markets_histories.history_date
            );
 
-DELETE FROM eve_markets_histories WHERE history_date < now() - interval '2 months';
+-- DELETE FROM eve_markets_histories;
 
-VACUUM FULL ANALYZE eve_markets_histories;
+-- VACUUM FULL ANALYZE eve_markets_histories;
