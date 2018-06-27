cb092605fdb1aa0832756ed2c22cd49913f5a227
ced <zuger.cedric@gmail.com>
2018-06-27 11:56:23 +0200

    Migration issue.

--- work/20150408144002_create_trade_hubs.rb
+++ work/20150408144002_create_trade_hubs.rb
@@ -1,10 +1,10 @@
-class CreateTradeHubs < ActiveRecord::Migration[4.2]
-  def change
-    create_table :trade_hubs do |t|
-      t.integer :eve_system_id
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
+# class CreateTradeHubs < ActiveRecord::Migration[4.2]
+#   def change
+#     create_table :trade_hubs do |t|
+#       t.integer :eve_system_id
+#       t.string :name
+#
+#       t.timestamps
+#     end
+#   end
+# end
