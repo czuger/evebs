1d87f5d7effae2d2b6ebae52e5fccbb12dfb8b10
ced <zuger.cedric@gmail.com>
2018-06-28 21:14:53 +0200

    Version changed.

--- app/models/bpc_prices_min.rb
+++ app/models/bpc_prices_min.rb
@@ -3,6 +3,8 @@ class BpcPricesMin < ApplicationRecord
   belongs_to :blueprint_component
 
   def self.feed_table
+    Banner.p 'About to update update_bpc_prices_mins'
+
     request = File.open( "#{Rails.root}/sql/update_bpc_prices_mins.sql" ).read
     ActiveRecord::Base.connection.execute( request )
   end
