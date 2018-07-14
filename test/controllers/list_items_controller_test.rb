101b65bcf5a7c12f3adecbd40fe59488681d3925
ced <zuger.cedric@gmail.com>
2018-07-14 10:45:58 +0200

    list_items_controller_test.

--- test/controllers/list_items_controller_test.rb
+++ test/controllers/list_items_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class ListItemsControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get list_items_edit_url
+
+  def setup
+    @user = create( :user, last_changes_in_choices: Time.now - 120 )
+    post '/auth/developer/callback', params: { name: @user.name }
+    @user.reload
+  end
+
+  test 'should get edit' do
+    get edit_list_items_url
     assert_response :success
   end
 
-  test "should get update" do
-    get list_items_update_url
-    assert_response :success
+  test 'should get update' do
+    patch list_items_url, params: { items: { 1 => :dummy } }
+    assert_redirected_to edit_list_items_url
   end
 
 end
