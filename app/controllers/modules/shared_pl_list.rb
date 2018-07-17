894e698ea6d0a30e1b70dd512f5fa600a007bf73
ced <zuger.cedric@gmail.com>
2018-07-17 09:09:00 +0200

    Fixing tests.

--- app/controllers/modules/shared_pl_list.rb
+++ app/controllers/modules/shared_pl_list.rb
@@ -2,8 +2,6 @@ module Modules::SharedPlList
 
   def set_user_to_show( user )
     @shared_production_list = user.current_character.character_pl_share
-    pp user.current_character
-    pp @shared_production_list
     @shared_production_list ? @shared_production_list.user : user
   end
 end
--- test/factories/regions.rb
+++ test/factories/regions.rb
@@ -9,25 +9,25 @@ FactoryBot.define do
       cpp_region_id '10000030'
       name 'Heimatar'
 
-      after(:create) do |region|
-
-        pator = create( :rens, region: region )
-        rens = create( :pator, region: region )
-
-        blueprint_and_market_group = create( :inferno_fury_cruise_missile )
-        blueprint_but_no_market_group = create( :mjolnir_fury_cruise_missile )
-        no_market_group_and_no_blueprint = create( :inferno_precision_cruise_missile )
-
-        [ blueprint_and_market_group, blueprint_but_no_market_group, no_market_group_and_no_blueprint ].each do |item|
-
-          [ pator, rens ].each do |trade_hub|
-            create( :min_price, trade_hub: trade_hub, eve_item: item )
-          end
-
-          create( :crest_prices_last_month_average, eve_item: item, region: region )
-
-        end
-      end
+      # after(:create) do |region|
+      #
+      #   pator = create( :rens, region: region )
+      #   rens = create( :pator, region: region )
+      #
+      #   blueprint_and_market_group = create( :inferno_fury_cruise_missile )
+      #   blueprint_but_no_market_group = create( :mjolnir_fury_cruise_missile )
+      #   no_market_group_and_no_blueprint = create( :inferno_precision_cruise_missile )
+      #
+      #   [ blueprint_and_market_group, blueprint_but_no_market_group, no_market_group_and_no_blueprint ].each do |item|
+      #
+      #     [ pator, rens ].each do |trade_hub|
+      #       create( :min_price, trade_hub: trade_hub, eve_item: item )
+      #     end
+      #
+      #     create( :crest_prices_last_month_average, eve_item: item, region: region )
+      #
+      #   end
+      # end
 
     end
 
