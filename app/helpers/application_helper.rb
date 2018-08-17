dbf35b61a71e4e812dedde823e6a19e48a1b1f17
ced <zuger.cedric@gmail.com>
2018-08-17 16:33:41 +0200

    First menu working.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
-  def active_link( path = root_path )
-    'active' if current_page?( path )
+  def active_link( class_string, path = root_path )
+    class_string += ' active' if current_page?( path )
+    class_string
   end
 
 end
