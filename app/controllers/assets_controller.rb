12c44a2518f33f34c538788210026c55b0364baf
ced <zuger.cedric@gmail.com>
2018-08-17 15:58:16 +0200

    Station form selection in Asset controller.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -5,6 +5,7 @@ class AssetsController < ApplicationController
 
   def show
     @assets = @user.bpc_assets.includes( :station_detail, :blueprint_component )
+    @stations_select_data = @assets.map{ |e| [ e.station_detail&.name, e.station_detail_id ] if e.station_detail_id }.uniq.compact.sort
   end
 
 end
