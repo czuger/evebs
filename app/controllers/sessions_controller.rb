38a74a6b74770e63317ecafbdaa42d0a9a9f48f4
ced <zuger.cedric@gmail.com>
2018-08-28 11:33:26 +0200

    Login now redirect to buy orders.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(request.env['omniauth.auth'])
     raise "User can't be found : #{user}" unless user
     session[:user_id] = user.id
-    redirect_to price_advices_advice_prices_url
+    redirect_to buy_orders_path
   end
 
   def destroy
--- sql/update_buy_ordrers_analytics.sql
+++ sql/update_buy_ordrers_analytics.sql
@@ -34,17 +34,3 @@ UPDATE buy_orders_analytics boa SET ( single_unit_cost, single_unit_margin, esti
 
 -- Avant de faire fonctionner ça il faut rajouter une colonne prix à l'unité dans eve item
 -- Il faudra également la suprimer de prices advices, il faut une source unique.
-SELECT boa.id, u.id user_id, boa.trade_hub_id, boa.eve_item_id, tu.name, ei.name, approx_max_price,
-  single_unit_cost, single_unit_margin, final_margin,
-  per_job_margin, CEIL( final_margin / per_job_margin ) job_count,
-  per_job_run_margin, FLOOR( final_margin / per_job_run_margin ) runs,
-  FLOOR( final_margin / per_job_run_margin ) * per_job_run_margin true_margin,
-  estimated_volume_margin, over_approx_max_price_volume
-FROM buy_orders_analytics boa
-  JOIN eve_items ei ON ei.id = boa.eve_item_id
-  JOIN trade_hubs tu ON boa.trade_hub_id = tu.id
-  JOIN trade_hubs_users thu ON boa.trade_hub_id = thu.trade_hub_id
-  JOIN eve_items_users eiu ON boa.eve_item_id = eiu.eve_item_id
-  JOIN users u ON thu.user_id = u.id AND eiu.user_id = u.id
-WHERE final_margin > 0
-ORDER BY final_margin DESC;
