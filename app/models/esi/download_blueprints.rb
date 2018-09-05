e96ce3a1c0cfca04cec8f6bfa4d41ef1a53f2a27
ced <zuger.cedric@gmail.com>
2018-09-05 07:10:33 +0200

    Download blueprints and parse blueprint separated.

---
+++ app/models/esi/download_blueprints.rb
@@ -0,0 +1,55 @@
+module Esi
+
+  class DownloadBlueprints < Download
+
+    def check
+      Banner.p 'About to download blueprints extra data'
+
+      blueprints = YAML::load_file('data/parsed_blueprints.yaml')
+      new_blueprints_list = []
+      eve_items_to_check_cpp_ids = Set.new
+
+      if @verbose_output
+        puts "#{blueprints.count} blueprints to check."
+        check_count = 0
+      end
+
+      blueprints.each do |bp|
+
+        if @verbose_output
+          check_count += 1
+          puts "#{check_count} blueprints checked" if check_count % 100 == 0
+        end
+
+        # pp bp
+
+        @rest_url = "universe/types/#{bp[:cpp_blueprint_id]}/"
+
+        begin
+          bp_remote_data = get_page
+
+        rescue Esi::Errors::NotFound
+          puts "Data not found for blueprint id : #{bp[:cpp_blueprint_id]}"
+          next
+        end
+
+        next unless bp_remote_data['published']
+
+        bp['name'] = bp_remote_data['name']
+        new_blueprints_list << bp
+
+        eve_items_to_check_cpp_ids = add_items_to_check( eve_items_to_check_cpp_ids, bp )
+      end
+
+      File.open('data/parsed_blueprints.yaml', 'w') {|f| f.write new_blueprints_list.to_yaml }
+      File.open('data/type_ids_to_download.yaml', 'w') {|f| f.write eve_items_to_check_cpp_ids.to_a.to_yaml }
+    end
+
+    def add_items_to_check( eve_items_to_check_cpp_ids, bp )
+      eve_items_to_check_cpp_ids << bp[:produced_cpp_type_id]
+      eve_items_to_check_cpp_ids += bp[:materials].map{ |m| m[:type_id] }
+      eve_items_to_check_cpp_ids
+    end
+
+  end
+end
--- app/models/esi/parse_blueprints_file.rb
+++ app/models/esi/parse_blueprints_file.rb
@@ -1,13 +1,6 @@
 module Esi
 
-  class ParseBlueprintsFile < Download
-
-    def process
-      parse
-      check
-    end
-
-    private
+  class ParseBlueprintsFile
 
     def parse
 
@@ -30,7 +23,7 @@ module Esi
           next unless products
 
           if products.count > 1
-            puts 'Blueprint produce more than one item'
+            puts 'Blueprint produce more than one item.'
             pp bp
             next
           end
@@ -44,51 +37,6 @@ module Esi
       end
 
       File.open('data/parsed_blueprints.yaml', 'w') {|f| f.write final_blueprints_list.to_yaml }
-    end
-
-    def check
-      Banner.p 'About to check blueprints and get names'
-
-      blueprints = YAML::load_file('data/parsed_blueprints.yaml')
-      new_blueprints_list = []
-      eve_items_to_check_cpp_ids = Set.new
-
-      puts "#{blueprints.count} blueprints to check."
-      check_count = 0
-
-      blueprints.each do |bp|
-
-        check_count += 1
-        puts "#{check_count} blueprints checked" if check_count % 100 == 0
-
-        # pp bp
-
-        @rest_url = "universe/types/#{bp[:cpp_blueprint_id]}/"
-
-        begin
-          bp_remote_data = get_page
-
-        rescue Esi::Errors::NotFound
-          puts "Data not found for blueprint id : #{bp[:cpp_blueprint_id]}"
-          next
-        end
-
-        next unless bp_remote_data['published']
-
-        bp['name'] = bp_remote_data['name']
-        new_blueprints_list << bp
-
-        eve_items_to_check_cpp_ids = add_items_to_check( eve_items_to_check_cpp_ids, bp )
-      end
-
-      File.open('data/parsed_blueprints.yaml', 'w') {|f| f.write new_blueprints_list.to_yaml }
-      File.open('data/type_ids_to_download.yaml', 'w') {|f| f.write eve_items_to_check_cpp_ids.to_a.to_yaml }
-    end
-
-    def add_items_to_check( eve_items_to_check_cpp_ids, bp )
-      eve_items_to_check_cpp_ids << bp[:produced_cpp_type_id]
-      eve_items_to_check_cpp_ids += bp[:materials].map{ |m| m[:type_id] }
-      eve_items_to_check_cpp_ids
     end
 
   end
