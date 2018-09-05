4bb0d14d41244c264e3f07283b63c1ccdf4bfa2a
ced <zuger.cedric@gmail.com>
2018-09-05 16:09:01 +0200

    UpdateEveItems store lowest production level.

--- app/models/process/update_eve_items.rb
+++ app/models/process/update_eve_items.rb
@@ -7,6 +7,8 @@ module Process
 
       types = YAML::load_file('data/types.yaml')
 
+      lowest_production_level = 0
+
       @blueprint_cpp_to_syntetic_key_conversion_hash = Hash[ Blueprint.pluck( :produced_cpp_type_id, :id ) ]
 
       types_to_destroy = EveItem.pluck( :cpp_eve_item_id ) - types.keys
@@ -20,10 +22,13 @@ module Process
         on_db_item.blueprint_id = @blueprint_cpp_to_syntetic_key_conversion_hash[type[:cpp_eve_item_id]]
 
         on_db_item.production_level = type[:production_level]
+        lowest_production_level = [ lowest_production_level, type[:production_level] ].min
         on_db_item.base_item = type[:base_item]
 
         on_db_item.save
       end
+
+      File.open('data/lowest_production_level', 'w') {|f| f.write lowest_production_level }
     end
   end
 
