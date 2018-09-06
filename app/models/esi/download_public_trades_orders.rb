b82f6f4420ddacfd5e0cdd947a00ae1447eadefe
ced <zuger.cedric@gmail.com>
2018-09-06 15:58:21 +0200

    Download public trade orders uses json stream instead of yaml.

--- app/models/esi/download_public_trades_orders.rb
+++ app/models/esi/download_public_trades_orders.rb
@@ -9,27 +9,29 @@ class Esi::DownloadPublicTradesOrders < Esi::Download
 
     orders = []
 
-    Region.all.each do |region|
+    File.open('data/public_trades_orders.json_stream', 'w') do |f|
 
-      if @verbose_output
-        puts "About to process region : #{region.name}"
-      end
+      Region.all.each do |region|
 
-      @rest_url = "markets/#{region.cpp_region_id}/orders/"
-      orders_data = get_all_pages
+        if @verbose_output
+          puts "About to process region : #{region.name}"
+        end
 
-      orders_data.each do |order_data|
-        next unless trade_hubs_ids.include?( order_data['system_id'] ) && eve_items_ids.include?( order_data['type_id'] )
+        @rest_url = "markets/#{region.cpp_region_id}/orders/"
+        orders_data = get_all_pages
 
-        orders << { duration: order_data['duration'], is_buy_order: order_data['is_buy_order'], issued: order_data['issued'],
-                    min_volume: order_data['min_volume'], order_id: order_data['order_id'], price: order_data['price'],
-                    range: order_data['range'], system_id: order_data['system_id'], type_id: order_data['type_id'],
-                    volume_remain: order_data['volume_remain'], volume_total: order_data['volume_total'] }
+        orders_data.each do |order_data|
+          next unless trade_hubs_ids.include?( order_data['system_id'] ) && eve_items_ids.include?( order_data['type_id'] )
 
+          order = { duration: order_data['duration'], is_buy_order: order_data['is_buy_order'], issued: order_data['issued'],
+                      min_volume: order_data['min_volume'], order_id: order_data['order_id'], price: order_data['price'],
+                      range: order_data['range'], system_id: order_data['system_id'], type_id: order_data['type_id'],
+                      volume_remain: order_data['volume_remain'], volume_total: order_data['volume_total'] }
+
+          f.puts( order.to_json )
+
+        end
       end
     end
-
-    File.open('data/public_trades_orders.yaml', 'w') {|f| f.write orders.to_yaml }
-
   end
 end
