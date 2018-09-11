a4193bb74c5bbd680d4306fda56fd2111db20f8a
ced <zuger.cedric@gmail.com>
2018-09-11 19:42:07 +0200

    Buy order list now have a production shortcut.

--- app/controllers/buy_orders_controller.rb
+++ app/controllers/buy_orders_controller.rb
@@ -3,7 +3,10 @@ class BuyOrdersController < ApplicationController
   before_action :require_logged_in!, :log_client_activity
   before_action :set_user
 
+  include Modules::CheckedProductionListIds
+
   def show
+    set_checked_production_list_ids
     @buy_orders = @user.buy_orders_analytics_results.paginate(:page => params[:page], :per_page => 20 )
   end
 
