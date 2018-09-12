1833fdf1676c57de5bbc955a20501a8f27c01688
ced <zuger.cedric@gmail.com>
2018-09-12 08:51:51 +0200

    Deleting old sales finals now print count correctly.

--- app/models/process/delete_old_sales_finals.rb
+++ app/models/process/delete_old_sales_finals.rb
@@ -7,7 +7,7 @@ module Process
       Banner.p 'About to delete old sales finals.'
 
       sales_to_delete = SalesFinal.where( 'created_at < ?', Time.now - 1.month )
-      puts "#{sales_to_delete} sales_final deleted."
+      puts "#{sales_to_delete.count} sales_final deleted."
       sales_to_delete.delete_all
     end
   end
