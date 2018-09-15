2d8c4a9bba88f838acbdf651f03d11dbd77aed9d
ced <zuger.cedric@gmail.com>
2018-09-15 03:55:56 +0200

    Root page is no more shown when user already logged.

--- app/controllers/jita_margins_controller.rb
+++ app/controllers/jita_margins_controller.rb
@@ -2,10 +2,14 @@ class JitaMarginsController < ApplicationController
 
   before_action :log_client_activity
   before_action :require_logged_in!, except: [ :index ]
+  before_action :set_user
 
   caches_page :index
 
   def index
+    if @user
+      redirect_to buy_orders_path
+    end
     # @margins = JitaMargin.includes( :eve_item )
     #   .where( 'margin_percent > 0.3' ).where( 'mens_volume > ( batch_size * 600 )' )
     #   .order( 'margin DESC' ).limit( 5 )
