cef3e96e810af9115b23bde94a659492411e84fb
ced <zuger.cedric@gmail.com>
2018-09-24 09:45:12 +0200

    Refresh sitemap is now ran after the weekly process.

--- scripts/refresh_sitemap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-cd $1
-
-RAILS_ENV=production bundle exec rake -s sitemap:refresh
--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -10,4 +10,6 @@ date > log/weekly.err
 
 RAILS_ENV=production bundle exec rake process:full:weekly >>log/weekly.log 2>>log/weekly.err
 
+RAILS_ENV=production bundle exec rake -s sitemap:refresh
+
 cat log/weekly.log log/weekly.err | mail `cat config/email.txt` -s "Evebs weekly process"
