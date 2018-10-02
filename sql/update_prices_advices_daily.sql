db81eb5b5c26647232af046179adbaf8f06dcd23
ced <zuger.cedric@gmail.com>
2018-10-02 17:09:30 +0200

    Removing non blueprints linked items in prices advices.

--- sql/update_prices_advices_daily.sql
+++ sql/update_prices_advices_daily.sql
@@ -5,14 +5,23 @@
 --     SELECT 1 FROM eve_items ei
 --     WHERE pm.eve_item_id = ei.id );
 
+DELETE FROM prices_advices
+  WHERE EXISTS (
+      SELECT 1 FROM prices_advices pa, eve_items ei
+      WHERE pa.eve_item_id = ei.id
+      AND ei.blueprint_id IS NULL
+  );
+
 /* Insert new regions / items */
 INSERT INTO prices_advices( eve_item_id, trade_hub_id, created_at, updated_at )
    SELECT DISTINCT sf.eve_item_id, sf.trade_hub_id, now(), now()
-   FROM sales_finals sf
-       WHERE NOT EXISTS (
-     SELECT 1 FROM prices_advices pa
-     WHERE sf.eve_item_id = pa.eve_item_id
-       AND sf.trade_hub_id = pa.trade_hub_id );
+   FROM sales_finals sf, eve_items ei
+     WHERE ei.id = sf.eve_item_id
+      AND ei.blueprint_id IS NOT NULL
+      AND NOT EXISTS (
+        SELECT 1 FROM prices_advices pa, eve_items ei
+        WHERE sf.eve_item_id = pa.eve_item_id
+        AND sf.trade_hub_id = pa.trade_hub_id );
 
 UPDATE prices_advices pa SET ( vol_month, avg_price_month, updated_at ) = ( mp, ap, now() )
 FROM (
