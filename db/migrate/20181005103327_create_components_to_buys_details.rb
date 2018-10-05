ee8eae3d36519bd7a626d0eea567f5a44255717c
ced <zuger.cedric@gmail.com>
2018-10-05 12:54:22 +0200

    Removing old component view.

--- db/migrate/20181005103327_create_components_to_buys_details.rb
+++ db/migrate/20181005103327_create_components_to_buys_details.rb
@@ -1,5 +1,6 @@
 class CreateComponentsToBuysDetails < ActiveRecord::Migration[5.2]
   def change
     create_view :components_to_buys_details
+    drop_view :component_to_buys, revert_to_version: 10
   end
 end
