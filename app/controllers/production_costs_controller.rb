5d506d642145f7d8eb06da72613e73db3a2782a6
ced <zuger.cedric@gmail.com>
2018-11-12 10:44:00 +0100

    Production cost updated.

--- app/controllers/production_costs_controller.rb
+++ app/controllers/production_costs_controller.rb
@@ -1,9 +1,11 @@
 class ProductionCostsController < ApplicationController
 
-  before_action :set_user
+  before_action :set_user, :set_small_screen, :set_show_update_daily
+
   caches_page :show, :dailies_avg_prices
 
   def show
+    @title = 'Production cost estimation'
     @item = EveItem.find_by( id: params[ :id ] )
     raise 'Base item cost should call dailies_avg_price instead of show' if @item.base_item
   end
@@ -11,6 +13,7 @@ class ProductionCostsController < ApplicationController
   # We put this here to ease the cache management
   def dailies_avg_prices
     @item = EveItem.find_by( id: params[ :item_id ] )
+    @title = @item.base_item ? 'Sales details included in price computation ' : 'Weekly price detail'
     @dailies_details = @item.weekly_price_details.where(trade_hub_id: params[:trade_hub_id]).order( 'day DESC' ).paginate( :page => params[:page] )
   end
 
