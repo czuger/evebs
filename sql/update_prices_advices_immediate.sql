b9885d634d192514e0c1bd8700f83368e0c02012
ced <zuger.cedric@gmail.com>
2018-11-13 11:55:11 +0100

    Handling infinity issues in costs computing.

--- sql/update_prices_advices_immediate.sql
+++ sql/update_prices_advices_immediate.sql
@@ -8,11 +8,23 @@ WHERE NOT EXISTS (
 
 UPDATE prices_advices pa SET ( margin_percent, immediate_montly_pcent, updated_at ) = ( min_so.margin_percent, daily_monthly_pcent, now() )
 FROM (
+       SELECT -1 margin_percent, -1 daily_monthly_pcent, sub_pa.trade_hub_id ti, sub_pa.eve_item_id ei
+       FROM prices_advices sub_pa
+         JOIN prices_mins pm ON pm.eve_item_id = sub_pa.eve_item_id AND pm.trade_hub_id = sub_pa.trade_hub_id
+         JOIN eve_items ei ON ei.id = pm.eve_item_id
+         JOIN blueprints bp ON ei.blueprint_id = bp.id
+      WHERE cost = 'Infinity' ) min_so
+WHERE ti = pa.trade_hub_id
+AND ei = pa.eve_item_id;
+
+UPDATE prices_advices pa SET ( margin_percent, immediate_montly_pcent, updated_at ) = ( min_so.margin_percent, daily_monthly_pcent, now() )
+FROM (
        SELECT ( min_price / cost - 1 ) margin_percent,
          min_price / avg_price_month daily_monthly_pcent, sub_pa.trade_hub_id ti, sub_pa.eve_item_id ei
        FROM prices_advices sub_pa
          JOIN prices_mins pm ON pm.eve_item_id = sub_pa.eve_item_id AND pm.trade_hub_id = sub_pa.trade_hub_id
          JOIN eve_items ei ON ei.id = pm.eve_item_id
-         JOIN blueprints bp ON ei.blueprint_id = bp.id ) min_so
+         JOIN blueprints bp ON ei.blueprint_id = bp.id
+        WHERE cost != 'Infinity' ) min_so
 WHERE ti = pa.trade_hub_id
-      AND ei = pa.eve_item_id;
+AND ei = pa.eve_item_id;
