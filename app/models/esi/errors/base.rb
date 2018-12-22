f9f5065f7486ceb42892d6cd9f57de45f1845c8d
ced <zuger.cedric@gmail.com>
2018-12-22 07:04:53 +0100

    Handling new error : internal server error.

--- app/models/esi/errors/base.rb
+++ app/models/esi/errors/base.rb
@@ -6,23 +6,26 @@ class Esi::Errors::Base < RuntimeError
     return Esi::Errors::SocketError.new if exception.message =~ /SocketError/
 
     case exception.message
-    when '504 Gateway Timeout', '504 Gateway Time-out'
-      error = Esi::Errors::GatewayTimeout.new
-    when '502 Bad Gateway'
-      error = Esi::Errors::BadGateway.new
-    when '403 Forbidden'
-      error = Esi::Errors::Forbidden.new
-    when '420 status code 420'
-      error = Esi::Errors::ErrorLimited.new
-    when '404 Not Found'
-      error = Esi::Errors::NotFound.new
-    when 'Net::OpenTimeout'
-      error = Esi::Errors::OpenTimeout.new
-    when 'SocketError'
-      error = Esi::Errors::SocketError.new
-    when '503 Service Unavailable'
-      error = Esi::Errors::ServiceUnavailable.new
-    end
+
+      when '500 Internal Server Error'
+        error = Esi::Errors::GatewayTimeout.new
+      when '504 Gateway Timeout', '504 Gateway Time-out'
+        error = Esi::Errors::GatewayTimeout.new
+      when '502 Bad Gateway'
+        error = Esi::Errors::BadGateway.new
+      when '403 Forbidden'
+        error = Esi::Errors::Forbidden.new
+      when '420 status code 420'
+        error = Esi::Errors::ErrorLimited.new
+      when '404 Not Found'
+        error = Esi::Errors::NotFound.new
+      when 'Net::OpenTimeout'
+        error = Esi::Errors::OpenTimeout.new
+      when 'SocketError'
+        error = Esi::Errors::SocketError.new
+      when '503 Service Unavailable'
+        error = Esi::Errors::ServiceUnavailable.new
+      end
 
     error
   end
