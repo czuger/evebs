c91ee60ccf7d4a45ddef5a541fb5cec8d6b23dfa
ced <zuger.cedric@gmail.com>
2019-04-22 19:19:02 +0200

    Migrations fixed.

--- db/migrate/20180823140710_add_fk_to_user_sale_order.rb
+++ db/migrate/20180823140710_add_fk_to_user_sale_order.rb
@@ -1,11 +1,11 @@
 class AddFkToUserSaleOrder < ActiveRecord::Migration[5.2]
   def change
     drop_view :user_sale_order_details
-    add_foreign_key :user_sales_orders, :users
-    add_foreign_key :user_sales_orders, :eve_items
-    add_foreign_key :user_sales_orders, :trade_hubs
-    change_column :user_sales_orders, :created_at, :timestamp, null: false
-    change_column :user_sales_orders, :updated_at, :timestamp, null: false
-    change_column :user_sales_orders, :price, :float, null: false
+    add_foreign_key :user_sale_orders, :users
+    add_foreign_key :user_sale_orders, :eve_items
+    add_foreign_key :user_sale_orders, :trade_hubs
+    change_column :user_sale_orders, :created_at, :timestamp, null: false
+    change_column :user_sale_orders, :updated_at, :timestamp, null: false
+    change_column :user_sale_orders, :price, :float, null: false
   end
 end
--- db/migrate/20181008013156_remove_quantity_to_produce_from_production_list.rb
+++ db/migrate/20181008013156_remove_quantity_to_produce_from_production_list.rb
@@ -1,6 +1,6 @@
 class RemoveQuantityToProduceFromProductionList < ActiveRecord::Migration[5.2]
   def up
-    drop_view :components_to_buys_details, revert_to_version: 10
+    drop_view :component_to_buys, revert_to_version: 10
     remove_column :production_lists, :quantity_to_produce, :bigint
     change_column :production_lists, :runs_count, :integer, limit: 2
   end
@@ -8,7 +8,7 @@ class RemoveQuantityToProduceFromProductionList < ActiveRecord::Migration[5.2]
   def down
     add_column :production_lists, :quantity_to_produce, :bigint
     change_column :production_lists, :runs_count, :bigint, null: true
-    create_view :components_to_buys_details
+    create_view :component_to_buys
   end
 
 end
