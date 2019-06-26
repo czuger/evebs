25231f242dd09cc51fcc7671946b14e39cb9b68c
ced <zuger.cedric@gmail.com>
2019-06-26 22:28:32 +0200

    User sales orders already singular.

--- db/migrate/20180823092300_rename_user_sales_order.rb
+++ db/migrate/20180823092300_rename_user_sales_order.rb
@@ -1,5 +1,5 @@
 class RenameUserSalesOrder < ActiveRecord::Migration[5.2]
   def change
-    rename_table :user_sales_orders, :user_sale_orders
+    # rename_table :user_sales_orders, :user_sale_orders
   end
 end
