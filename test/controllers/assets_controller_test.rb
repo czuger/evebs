9ca554f8e15082ee673d74adec880c65161eec41
ced <zuger.cedric@gmail.com>
2019-07-09 07:24:20 +0200

    Improving tests.

--- test/controllers/assets_controller_test.rb
+++ test/controllers/assets_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class AssetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = create( :user )
+    post '/auth/developer/callback', params: { name: @user.name }
+
+    # @trade_hub = create( :jita )
+    @station = create( :station_detail )
+  end
+
+  test 'should show' do
+    get my_assets_path
+    assert_response :success
+  end
+
+  test 'should start downloading my assets' do
+    patch my_assets_path
+    assert_redirected_to my_assets_path
+  end
+
+  test 'should set my asset station id' do
+    post set_assets_station_my_assets_path, params: { asset_station_id: @station.id }
+    assert_redirected_to my_assets_path
+  end
+
+
 end
--- test/factories/stations.rb
+++ test/factories/stations.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :station do
 
-
     name {'MyString'}
     cpp_station_id {1}
   end
