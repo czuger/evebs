f25588ba863eade440ee4f1a65220848fb855575
ced <zuger.cedric@gmail.com>
2019-07-12 10:30:51 +0200

    Tests finished for JitaMarginsController.

--- test/controllers/jita_margins_controller_test.rb
+++ test/controllers/jita_margins_controller_test.rb
@@ -9,9 +9,17 @@ class JitaMarginsControllerTest < ActionDispatch::IntegrationTest
     # post '/auth/developer/callback', params: { name: @user.name }
   end
 
-  test 'should show front page' do
+  test 'should show front page when no user is connected' do
     get '/'
     assert_response :success
+  end
+
+  test 'should show front page when an user is connected' do
+    @user = create( :user )
+    post '/auth/developer/callback', params: { name: @user.name }
+
+    get '/'
+    assert_redirected_to buy_orders_url
   end
 
 end
