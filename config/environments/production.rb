e809e592a541ceafbf4bb921f52cc7b59c196cb7
ced <zuger.cedric@gmail.com>
2019-07-16 11:35:53 +0200

    Enabling uglifier for javascript.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,10 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = false
 
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
