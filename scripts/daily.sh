09ee90767af6c5327de53d2683777155319371cf
ced <zuger.cedric@gmail.com>
2019-08-13 15:38:01 +0200

    Daily script modified to be autonomous.

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -3,15 +3,15 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
-cd $1
+cd "`dirname $BASH_SOURCE`/.."
 
-date >> log/daily.log
+dte >> log/daily.log
 date >> log/daily.err
 
-RAILS_ENV=production bundle exec rake process:full:daily >>log/daily.log 2>>log/daily.err
+bundle exec rake process:full:daily >>log/daily.log 2>>log/daily.err
 
 # cat log/daily.log log/daily.err | mail `cat config/email.txt` -s "Evebs daily process"
 
-rm -rf public/production_costs
+#rm -rf public/production_costs
 
-RAILS_ENV=production bundle exec rake maintenance:check_working_hourly_process >>log/check_working_hourly_process.log 2>>log/check_working_hourly_process.err
+bundle exec rake maintenance:check_working_hourly_process >>log/check_working_hourly_process.log 2>>log/check_working_hourly_process.err
