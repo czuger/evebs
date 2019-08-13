736af74665383a60cd2a9eeccbbf4978de26a4b0
ced <zuger.cedric@gmail.com>
2019-08-13 16:04:57 +0200

    Hourly script no more bind with production environment.

--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -8,7 +8,7 @@ cd "`dirname $BASH_SOURCE`/.."
 date >> log/hourly.log
 date >> log/hourly.err
 
-RAILS_ENV=production bundle exec rake process:full:hourly >>log/hourly.log 2>>log/hourly.err
+bundle exec rake process:full:hourly >>log/hourly.log 2>>log/hourly.err
 
 # cat log/hourly.log log/hourly.err | mail `cat config/email.txt` -s "Evebs hourly process"
 
