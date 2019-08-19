5ee525c90c30f39c550e9ce1f940985811d1f114
ced <zuger.cedric@gmail.com>
2019-08-19 23:12:46 +0200

    Banner now print in stdout and stderr.

--- app/models/misc/banner.rb
+++ app/models/misc/banner.rb
@@ -2,10 +2,18 @@ module Misc
   class Banner
 
     def self.p( string )
-      puts '*'*100
-      puts string + ' - ' + Time.now.strftime( '%c')
-      puts '*'*100
+      out '*'*100
+      out string + ' - ' + Time.now.strftime( '%c')
+      out '*'*100
       STDOUT.flush
+      STDERR.flush
+    end
+
+    private
+
+    def self.out( string )
+      puts string
+      warn string
     end
 
   end
