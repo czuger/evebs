c6660cff4615c92923668afb16aaeb728fb04d9d
ced <zuger.cedric@gmail.com>
2019-08-19 16:13:55 +0200

    Loading error mailer now add an indication on environment.

--- app/mailers/load_error_mailer.rb
+++ app/mailers/load_error_mailer.rb
@@ -2,7 +2,7 @@ class LoadErrorMailer < ApplicationMailer
 
   def send_error
     @user = params[:user]
-    mail(to: @user[:email], subject: 'Erreur de chargement EVEBS') do |format|
+    mail(to: @user[:email], subject: "Erreur de chargement EVEBS sur l'environnement #{Rails.env}") do |format|
       format.text
     end
   end
