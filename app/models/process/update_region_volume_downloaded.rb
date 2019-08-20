dfe2f33669bfaef5b538f8470239ad77a9a5daad
ced <zuger.cedric@gmail.com>
2019-08-20 19:20:40 +0200

    UpdateRegionVolumeDownloaded now read from all produced files.

--- app/models/process/update_region_volume_downloaded.rb
+++ app/models/process/update_region_volume_downloaded.rb
@@ -8,21 +8,25 @@ module Process
       eve_item_conversion_hash ||= Hash[ EveItem.pluck( :cpp_eve_item_id, :id ) ]
 
       TradeVolumeEstimation.transaction do
-        File.open( 'data/regional_sales_volumes.json_stream', 'r' ) do |f|
-          f.each do |line|
-            data = JSON.parse( line )
+        1.upto(Esi::DownloadHistoryReadItemsLists::PROCESSES_COUNT).each do |process_id|
 
-            eve_item_id = eve_item_conversion_hash[data['cpp_type_id']]
-            next unless eve_item_id
+          File.open( "data/regional_sales_volumes_#{process_id}.json_stream", 'r' ) do |f|
+            f.each do |line|
+              data = JSON.parse( line )
 
-            systems = UniverseSystem.joins( { universe_constellation: :universe_region } ).
-                where( 'universe_regions.cpp_region_id = ?', data['cpp_region_id'] )
+              eve_item_id = eve_item_conversion_hash[data['cpp_type_id']]
+              next unless eve_item_id
 
-            TradeVolumeEstimation.
-                where( eve_item_id: eve_item_id ).
-                where( universe_system: systems ).
-                update_all( region_volume_downloaded: data['volume'] )
+              systems = UniverseSystem.joins( { universe_constellation: :universe_region } ).
+                  where( 'universe_regions.cpp_region_id = ?', data['cpp_region_id'] )
+
+              TradeVolumeEstimation.
+                  where( eve_item_id: eve_item_id ).
+                  where( universe_system: systems ).
+                  update_all( region_volume_downloaded: data['volume'] )
+            end
           end
+
         end
       end
     end
