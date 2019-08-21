bd0934f2b1672d2243f578c88c24530f7d23d322
ced <zuger.cedric@gmail.com>
2019-08-21 12:31:40 +0200

    Added a check for production level computation.

--- app/models/misc/check_items_productions_levels.rb
+++ app/models/misc/check_items_productions_levels.rb
@@ -2,19 +2,42 @@ module Misc
   class CheckItemsProductionsLevels
 
     def check
-      EveItem.each do |item|
-        sub_monitor( item, nil, 0 )
-        puts
+      @failures = 0
+      @logfile = File.open( 'log/items_productions_levels.err', 'w' )
+
+      @eve_items = Hash[ EveItem.all.map{ |e| [ e.id, e ] } ]
+
+      @eve_items.values.each do |item|
+        next if item.base_item
+        sub_check( item, 1, nil )
       end
+
+      if @failures >= 1
+        puts 'Failures found, please check logfile'
+      else
+        puts 'No failures found.'
+      end
+
+      @logfile.close
     end
 
     private
 
-    def sub_check( item, req_qtt, level )
-      puts ('%02d' % item.production_level) + ' > ' * level + " - #{item.name}(#{item.id}) - #{req_qtt} * #{item.cost ? item.cost : 'NULL'}"
+    def sub_check( item, parent_level, parent )
+      unless item.production_level
+        @logfile.puts "Production level for #{item.name}(#{item.id}) is null"
+        @failures += 1
+      else
+        if item.production_level >= parent_level
+          @logfile.puts "Production level for #{item.name}(#{item.id}) is over the parent's production level (#{parent.name}) : #{item.production_level} >= #{parent_level}"
+          @failures += 1
+        end
+      end
 
       item.blueprint_materials.each do |ma|
-        sub_monitor( ma.eve_item, ma.required_qtt, level+1 )
+        sub_item = @eve_items[ma.eve_item_id]
+        next if sub_item.base_item
+        sub_check( sub_item, item.production_level, item )
       end
     end
 
