8afb17ad0535f711fe8ab75539f9700395dda80b
ced <zuger.cedric@gmail.com>
2019-12-22 12:33:29 +0100

    Database now configured to use dockers databases on localhost.

--- docker/database-dev-test-compose-recreate.sh
+++ docker/database-dev-test-compose-recreate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker-compose -f database-dev-compose.yml stop
 docker-compose -f database-dev-compose.yml build
-docker-compose  -f database-dev-compose.yml up --force-recreate -d
+docker-compose  -f database-dev-compose.yml up --force-recreate --remove-orphans -d
