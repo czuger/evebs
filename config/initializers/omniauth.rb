65ce6c377757dd3eff837809ca769a327e48abe7
ced <zuger.cedric@gmail.com>
2019-12-22 14:13:09 +0100

    Tests are working even if omniauth.yml is missing.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,15 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   # Thanks to : https://coderwall.com/p/bsfitw/ruby-on-rails-4-authentication-with-facebook-and-omniauth
 
-  if File.exists?( 'config/omniauth.yaml' )
-    results = YAML.load( File.open( 'config/omniauth.yaml' ).read )
-
-    if results && results[:esi]
-      client_id, secret_key = results[:esi]
-      provider :eve_online_sso, client_id, secret_key, scope: 'esi-characters.read_blueprints.v1 esi-assets.read_assets.v1 esi-markets.read_character_orders.v1 esi-universe.read_structures.v1 esi-markets.structure_markets.v1'
-    end
-  end
-
+  id= File.exist?( 'config/omniauth.yaml' ) ? YAML.load_file( 'config/omniauth.yaml' ) : { esi: [] }
+  client_id, secret_key = id[:esi]
+  provider :eve_online_sso, client_id, secret_key, scope: 'esi-characters.read_blueprints.v1 esi-assets.read_assets.v1 esi-markets.read_character_orders.v1 esi-universe.read_structures.v1 esi-markets.structure_markets.v1'
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
