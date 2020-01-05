ebd9f9ecacbc10a55dc619b579c595301de5daf6
ced <zuger.cedric@gmail.com>
2020-01-05 16:36:29 +0100

    Old sql updated.

--- db/views/component_to_buys_v10.sql
+++ db/views/component_to_buys_v10.sql
@@ -14,3 +14,27 @@ FROM production_lists pl
 WHERE runs_count IS NOT NULL
 GROUP BY bpm_mat_ei.id, pl.user_id, bpm_mat_ei.name, COALESCE( ba.quantity, 0 )
 HAVING SUM( raw_qtt ) - COALESCE( ba.quantity, 0 ) > 0;
+
+-- This is not used anymore. See components_to_buy instead.
+-- Anyway, new version would be :
+
+SELECT bpm_mat_ei.id, pl.user_id, bpm_mat_ei.name,
+  SUM( raw_qtt ) - COALESCE( ba.quantity, 0 ) qtt_to_buy,
+  ( SUM( raw_qtt ) - COALESCE( ba.quantity, 0 ) )*bpm_mat_ei.cost total_cost,
+  ( SUM( raw_qtt ) - COALESCE( ba.quantity, 0 ) )*bpm_mat_ei.volume required_volume
+FROM production_lists pl
+  JOIN eve_items ei ON ei.id = pl.eve_item_id
+  JOIN blueprints b ON ei.blueprint_id = b.id
+  JOIN blueprint_materials bm ON b.id = bm.blueprint_id
+  JOIN eve_items bpm_mat_ei ON bm.eve_item_id = bpm_mat_ei.id
+  JOIN users ue ON pl.user_id = ue.id
+  LEFT JOIN blueprint_modifications bmo ON b.id = bmo.blueprint_id AND bmo.user_id = pl.user_id
+  LEFT JOIN bpc_assets ba ON bpm_mat_ei.id = ba.eve_item_id AND ba.universe_station_id = ue.selected_assets_station_id,
+  LATERAL (
+  SELECT required_qtt*runs_count*COALESCE( bmo.percent_modification_value, 1 ) * constants.f_value AS raw_qtt
+  FROM constants
+  ) qtt_comp
+WHERE runs_count IS NOT NULL
+GROUP BY bpm_mat_ei.id, pl.user_id, bpm_mat_ei.name, COALESCE( ba.quantity, 0 )
+HAVING SUM( raw_qtt ) - COALESCE( ba.quantity, 0 ) > 0;
+
